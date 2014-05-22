d39b44cc6452d4edda457cfd2fed2c5c33e277aa
zhoubangtao <zhoubangtao@163.com>
2014-05-22 10:41:25 +0800

    add shema.sql DDL and delete mass_mailer.sqlite, this can create with the schema.sql

---
+++ db/shema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE smtp_accounts
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    enable TEXT NOT NULL,
+    smtp_server_id INTEGER NOT NULL
+);
+CREATE TABLE smtp_servers
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    host TEXT NOT NULL,
+    port INTEGER NOT NULL,
+    postfix TEXT NOT NULL,
+    limit_per_day INTEGER,
+    limit_per_hour INTEGER,
+    limit_per_ip INTEGER,
+    limit_per_account INTEGER
+);
+CREATE UNIQUE INDEX name_index ON smtp_servers ( name );
